f20a9fff7616deec3fa7b2dbb4e6aa133e476fad
wkcn <wkcn@live.cn>
2018-01-02 10:23:02 +0800

    using ndarray

--- compute.py
+++ compute.py
@@ -1,40 +1,56 @@
 import numpy as np
 import mxnet as mx
 import mxnet.autograd as ag
+import mxnet.ndarray as nd
+from batchnorm import *
+
+ctx = mx.cpu(0)
+cudnn_off = True
 
 data = mx.nd.load("./d.params")
-mmean = data['aux:bn2a_branch1_moving_mean'].asnumpy().reshape((1, 256, 1, 1))
-mvar = data['aux:bn2a_branch1_moving_var'].asnumpy().reshape((1, 256, 1, 1))
-beta = data['arg:bn2a_branch1_beta'].asnumpy().reshape((1, 256, 1, 1))
-gamma = data['arg:bn2a_branch1_gamma'].asnumpy().reshape((1, 256, 1, 1))
+mmean = data['aux:bn2a_branch1_moving_mean'].reshape((1, 256, 1, 1))
+mvar = data['aux:bn2a_branch1_moving_var'].reshape((1, 256, 1, 1))
+beta = data['arg:bn2a_branch1_beta'].reshape((1, 256, 1, 1))
+gamma = data['arg:bn2a_branch1_gamma'].reshape((1, 256, 1, 1))
 
-x = np.arange(256 * 56 * 56).reshape((1, 256, 56, 56)).astype(np.float32)
-f = 1.0 / np.sqrt(mvar + 1e-5)
+x = mx.nd.arange(256 * 56 * 56).reshape((1, 256, 56, 56)).astype(np.float32)
+f = 1.0 / mx.nd.sqrt(mvar + 1e-5)
 
-w = (x - mmean) / np.sqrt(mvar + 1e-5)
-# w = (x - mmean) * f
+USING_DIV = True
+
+if USING_DIV:
+    w = (x - mmean) / mx.nd.sqrt(mvar + 1e-5)
+else:
+    w = (x - mmean) * f
 
 y = gamma * w + beta
 
 ty = np.load("./caffe.npy")
 
-h = mx.nd.array
+
+h = lambda x : mx.nd.array(x, ctx = ctx)
 hx = h(x)
 hx.attach_grad()
 
 with ag.record():
-    my, mxmean, mxvar = mx.nd.BatchNorm(data = hx, gamma = h(gamma), beta = h(beta), moving_mean = h(mmean), moving_var = h(mvar), eps = 1e-5, fix_gamma = False, use_global_stats = False, output_mean_var = True)
+    my, mxmean, mxvar = mx.nd.BatchNorm(data = hx, gamma = h(gamma), beta = h(beta), moving_mean = h(mmean), moving_var = h(mvar), eps = 1e-5, fix_gamma = False, use_global_stats = False, output_mean_var = True, cudnn_off = cudnn_off)
 
-my = mx.nd.BatchNorm(data = hx, gamma = h(gamma), beta = h(beta), moving_mean = h(mmean), moving_var = h(mvar), eps = 1e-5, fix_gamma = False, use_global_stats = True, output_mean_var = False)
+my = mx.nd.BatchNorm(data = hx, gamma = h(gamma), beta = h(beta), moving_mean = h(mmean), moving_var = h(mvar), eps = 1e-5, fix_gamma = False, use_global_stats = True, output_mean_var = False, cudnn_off = cudnn_off)
 
-my = my.asnumpy()
-mxmean = mxmean.asnumpy()
-mxvar = mxvar.asnumpy()
+ty = nd.array(ty, ctx = ctx)
 
-print ("caffe and numpy", np.max(np.abs(ty - y)), np.max(np.abs((ty - y) / y)) )
+print ("context: ", ctx)
+print ("cudnn_off: ", cudnn_off)
+if USING_DIV:
+    print ("ndarray: y = gamma * [(x - mean) / sqrt(var + eps)] + beta")
+else:
+    print ("ndarray: y = gamma * [(x - mean) * (1.0 / sqrt(var + eps)) ] + beta")
 
-print ("caffe and mx", np.max(np.abs(ty - my)), np.max(np.abs((ty - my) / y)) ) 
-print ("numpy and mx", np.max(np.abs(y - my)), np.max(np.abs((y - my) / y)) )
+print ("")
 
-# print ("diff mean", np.max(np.abs(mxmean - np.mean(x, (0,2,3)))))
-# print ("diff var", np.max(np.abs(mxvar - np.var(x, (0,2,3)) )))
+print ("                max absolute error, max relative error, mean error")
+
+print ("caffe and ndarray", nd.max(nd.abs(ty - y)).asscalar(), nd.max(nd.abs((ty - y) / y)).asscalar(), nd.mean(nd.abs(ty - y)).asscalar())
+
+print ("caffe and mx", nd.max(nd.abs(ty - my)).asscalar(), nd.max(nd.abs((ty - my) / y)).asscalar(), nd.mean(nd.abs(ty - my)).asscalar())
+print ("ndarray and mx", nd.max(nd.abs(y - my)).asscalar(), nd.max(nd.abs((y - my) / y)).asscalar(), nd.mean(nd.abs(y - my)).asscalar())
